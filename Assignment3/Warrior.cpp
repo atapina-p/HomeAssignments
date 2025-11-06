c079e48242c7193745b9151f1c45b49c5942ef63
atapina-p <st139859@spbu.ru>
2025-11-06 17:58:27 +0300

    an inherited Warrior class has been developed, not tested

---
+++ Assignment3/Warrior.cpp
@@ -0,0 +1,32 @@
+#include "Transformer.h"
+#include "Warrior.h"
+
+Warrior::Warrior(std::string name, int health, int energy, bool isTransformed, 
+    PrimaryWeapon weapon, int armor, int rage):
+    Transformer(name, health, energy, isTransformed, weapon),
+    _armor(armor),
+    _rage(rage){};
+
+
+void Warrior::furiousStrike(){
+    std::cout << _rage*getEnergy() << std::endl;
+}
+
+
+// Getters
+int Warrior::getArmor(){
+    return _armor;
+}
+int Warrior::getRage(){
+    return _rage;
+}
+
+
+// Setters
+void Warrior::setArmor(int newArmor){
+    _armor = newArmor;
+}
+void Warrior::setRage(int newRage){
+    _rage = newRage;
+}
+
---
+++ Assignment3/Warrior.h
@@ -0,0 +1,23 @@
+#ifndef WARRIOR
+#define WARRIOR
+#include <string>
+#include <iostream>
+#include "Transformer.h"
+
+class Warrior: public Transformer {
+    public:
+        Warrior(std::string name, int health, int energy, bool isTransformed, PrimaryWeapon weapon, int armor, int rage);
+
+        void furiousStrike();
+
+        // Getters
+        int getArmor();
+        int getRage();
+        // Setters
+        void setArmor(int newArmor);
+        void setRage(int newRage);
+    private:
+        int _armor;
+        int _rage;
+};
+#endif
