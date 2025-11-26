8ff52e6eb37652f92a3fc210737d74e16df1a6f7
atapina-p <st139859@spbu.ru>
2025-11-26 15:03:20 +0300

    fixed main code

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -14,6 +14,6 @@
 
 int main(){
     PrimaryWeapon weapon(25);
-    Scout scout("Optimus", 100, 100, false, weapon, 50, 10, 7);
+    Scout scout("Optimus", 100, 100, false, &weapon, 50, 10, 7);
     scout.scanArea();
 }
